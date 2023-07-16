2a22c106268bdc95c575a66af3ee44ac1dfb80b9
siddharthsingh <siddharthsingh25102001@gmail.com>
2023-07-16 22:54:11 +0530

    userController done and working on token

--- controllers/userController.go
+++ controllers/userController.go
@@ -3,7 +3,9 @@ package controllers
 import (
 	"context"
 	"example/RestaurantProject/database"
+	helpers "example/RestaurantProject/helpers"
 	"example/RestaurantProject/models"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
@@ -11,7 +13,9 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
@@ -92,23 +96,79 @@ func GetUser() gin.HandlerFunc {
 func SignUp() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
+
 		// convert the json data comming from postman to something that golang understands
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
 
 		//validate the data based on user struct
+		//to validate data that we get from user
+		//it uses tag we provide in our models to validate data { validator pkg}
+		validationErr := validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
 
 		//you'll check if the email has already been used by another user
 
+		count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while checking for the email"})
+			return
+		}
+
 		//hash password
 
+		password := HashPassword(*user.Password)
+		user.Password = &password
+
 		//we will also check if the phone no. has already has been used by another user
+		count, err = userCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while checking for the phone number"})
+			return
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "this email or phone number already exsists"})
+			return
+		}
 
 		// create  some extra details - created_at , updated_at ,ID
 
+		//timeStamps
+		user.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.ID = primitive.NewObjectID()
+		user.User_id = user.ID.Hex()
+
 		//genrate token and refersh token : (using func [genrateAllToken] from helper)
+
+		token, refreshToken, _ := helpers.GenrateAllToken(*user.Email, *user.First_name, *user.Last_name, user.User_id)
+		user.Token = &token
+		user.Refresh_Token = &refreshToken
 
 		// if all ok , then you insert this new user into the user collection
 
+		resultInsertionNumber, insertErr := userCollection.InsertOne(ctx, user)
+		if insertErr != nil {
+			msg := fmt.Sprintf("user were not created: something went wrong")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+		defer cancel()
+
 		//return status ok and send the result back
+		c.JSON(http.StatusOK, resultInsertionNumber)
 
 	}
 }
@@ -117,28 +177,67 @@ func Login() gin.HandlerFunc {
 
 	return func(c *gin.Context) {
 
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
+		var foundUser models.User
+
 		//unmarshal json data from postman
+		// convert the json data comming from postman to something that golang understands
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
 
 		// match user credential
-		// if user exist
+
+		err := userCollection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&foundUser)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "user not found : recheck your credential "})
+			return
+		}
 
 		//then we will veryfy password
 
+		passwordIsValid, msg := VerifyPassword(*user.Password, *foundUser.Password)
+		defer cancel()
+		if passwordIsValid != true {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+
 		//if all goes well , then we will genrate tokens
+		token, refreshToken, _ := helpers.GenrateAllToken(*&foundUser.Email, *&foundUser.First_name, *&foundUser.Last_name, foundUser.User_id)
 
 		//update tokens - token and refresh token
+		helpers.UpdateAllToken(token, refreshToken, foundUser.User_id)
 
 		//return status okay
-
+		c.JSON(http.StatusOK, foundUser)
 	}
 }
 
 func HashPassword(password string) string {
 
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		log.Panic(err)
+	}
+	return string(bytes)
 }
 
 func VerifyPassword(userPassword string, providePassword string) (bool, string) {
 
+	err := bcrypt.CompareHashAndPassword([]byte(providePassword), []byte(userPassword))
+
+	check := true
+	msg := ""
+
+	if err != nil {
+		msg = fmt.Sprint("user or passward is incorrect")
+		check = false
+	}
+	return check, msg
 }
 
 //we are using JWT authentication
--- helpers/tokenHelper.go
+++ helpers/tokenHelper.go
@@ -1,6 +1,44 @@
 package helpers
 
-func GenrateAllToken() {
+import (
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+type SignedDetails struct {
+	Email      string
+	First_name string
+	Last_name  string
+	Uid        string
+	jwt.StandardClaims
+}
+
+var SECRET_KEY string = os.Getenv("SECRET_KEY")
+
+func GenrateAllToken(email string, firstName string, lastName string, uid string) (signedToken string, refreshToken string, err error) {
+
+	//we use this data to create token
+	claims := &SignedDetails{
+		Email:      email,
+		First_name: firstName,
+		Last_name:  lastName,
+		Uid:        uid,
+		StandardClaims: jwt.StandardClaims{ //token expire in 24 hours , standard time is 30min
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(24)).Unix(),
+		},
+	}
+
+	refreshClaims := &SignedDetails{
+
+		StandardClaims: jwt.StandardClaims{ //token expire in 24 hours , standard time is 30min
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(100)).Unix(),
+		},
+	}
+
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(SECRET_KEY))
+	refershToken, err := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshClaims).SignedString([]byte(SECRET_KEY))
 
 }
 
